24aa0f96f4e72e96f618ddcde026eb8d1de7bb84
Alexandre Negrel <alexandre@negrel.dev>
2026-03-19 20:47:45 +0100

    add a function to measure system timer precision

---
+++ src/timer.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+const hint = @import("hint.zig");
+
+/// Timer precision is the smallest nonzero time interval measurable by the
+/// timer.
+pub fn measurePrecision() std.time.Timer.Error!u64 {
+    var min_sample: u64 = std.math.maxInt(u64);
+    var seen_count: usize = 0;
+    var delay_iter: usize = 0;
+
+    var timer: std.time.Timer = try std.time.Timer.start();
+
+    while (true) {
+        var prec_sample: usize = undefined;
+        for (0..100) |_| {
+            if (delay_iter == 0) {
+                timer.reset();
+                prec_sample = timer.read();
+            } else {
+                timer.reset();
+                for (0..delay_iter) |i| {
+                    hint.blackHole(i);
+                }
+                prec_sample = timer.read();
+            }
+
+            if (prec_sample == 0) continue;
+
+            switch (std.math.order(prec_sample, min_sample)) {
+                .lt => {
+                    min_sample = prec_sample;
+                    seen_count = 0;
+                },
+                .eq => {
+                    seen_count += 1;
+                    if (seen_count >= 100) return min_sample;
+                },
+                .gt => {
+                    if (delay_iter > 100) return min_sample;
+                },
+            }
+        }
+
+        delay_iter += 1;
+    }
+}
+
+test "measurePrecision" {
+    const prec = try measurePrecision();
+    try std.testing.expect(prec > 0);
+}
