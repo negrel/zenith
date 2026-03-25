6495d71d15277792bae129890e9154d63b03d843
Alexandre Negrel <alexandre@negrel.dev>
2026-03-25 13:19:25 +0100

    build: generate docs

--- build.zig
+++ build.zig
@@ -58,4 +58,14 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&install_lib_unit_tests.step);
+
+    // Generate documentation.
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+
+    const docs_step = b.step("docs", "Install docs into zig-out/docs");
+    docs_step.dependOn(&install_docs.step);
 }
