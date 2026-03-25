0ac653158aa6aee182e005b324e5087f10de2b25
Alexandre Negrel <alexandre@negrel.dev>
2026-03-25 13:44:39 +0100

    example: add simple example

--- examples/fib/build.zig
+++ examples/fib/build.zig
@@ -5,19 +5,11 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const bench = b.option(bool, "bench", "Run zenith benchmarks") orelse false;
 
-    const mod = b.addModule("test", .{
+    const mod = b.addModule("fib", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const zenith = b.dependency("zenith", .{
-        .run = bench,
-        .target = target,
-        .optimize = optimize,
-    });
-
-    mod.addImport("zenith", zenith.module("zenith"));
 
     const mod_tests = b.addTest(.{
         .root_module = mod,
@@ -27,4 +19,12 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
+
+    if (b.lazyDependency("zenith", .{
+        .target = target,
+        .optimize = optimize,
+        .run = bench, // See build options section below.
+    })) |dep| {
+        mod_tests.root_module.addImport("zenith", dep.module("zenith"));
+    }
 }
